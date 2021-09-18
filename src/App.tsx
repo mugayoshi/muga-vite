44ce39cbbba29208fc224e3d4fbb6ea5d67257f2
mugayoshi <muga.y.j3039@gmail.com>
2021-09-18 23:43:59 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className="bg-gray-700">Hello world</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  mode: "jit", // Just in time compiler
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
